d326e933f76e51ca0b59c87108ec0ff7e99f1836
mahendra9119 <mahenderbhakar9119@gmail.com>
2025-06-01 10:03:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api_id"
-api_hash = "enter your api_hash"
-bot_token = "enter your bot token"
+api_id = "28021723"
+api_hash = "f6b0736c56e8544e6d55b7c4ef169a68"
+bot_token = "8170387087:AAGIctE5k2wtCPgUyWSgTloNnShneEc8XuU"
 auth_users = [6312174113]
